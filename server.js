6fbb0cdcb45082373503e43018286c419190ff4d
Huan <e1700708@edu.vamk.fi>
2023-04-23 12:10:56 +0700

    update style

--- server.js
+++ server.js
@@ -3,22 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// const forceSSL = function () {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//         ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// };
 app.use(express.static('./dist/thesisfev2'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,'/dist/thesisfev2/index.html'));
 });
-
-// app.use(forceSSL());
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
